3c4490440534dc9404edf2c06e4c6643b09d0c76
blaktigger <69227197+blaktigger@users.noreply.github.com>
2020-08-28 18:53:58 +0900

    name

--- greetings.js
+++ greetings.js
@@ -1,27 +1,37 @@
 const form= document.querySelector(".js-form"),
-     input=form.querySelector("input"),
-     greeting =document.querySelector(".js-greetings")
+     input= form.querySelector("input"),
+     greeting= document.querySelector(".js-greetings")
 
 
 const USER_LS = "currentUser",
       SHOWING_CN="showing";
 
+function handleSubmit(event){
+    
+}
+
+function askForName(){
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit")
+}
+
 function paintGreeting(text){
-    greeting.classList.add(SHOWING_CN)
-    greeting.innerText= `Hello ${text}`
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText= `Hello ${text}`;
 }
 
 function loadName(){
     const currentUser=localStorage.getItem(USER_LS);
     if(currentUser===null){ 
-
+        askForName();
     } else{
         paintGreeting(currentUser);
     }
 }
 
 function init(){
-
+    loadName();
 }
 init();  
 
